9e88112190e73ebd60713ab1fa7184cd0b4e41a3
yossy_zip <mattsu.zip@gmail.com>
2021-05-11 13:19:13 +0900

    initial commit

---
+++ src/main/java/com/yiorno/mofubossbattle/MOFUBossBattle.java
@@ -0,0 +1,17 @@
+package com.yiorno.mofubossbattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MOFUBossBattle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
